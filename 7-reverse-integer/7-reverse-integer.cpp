4cf9b6af3313f5e5b6189ca87cb26511b30a542b
Anand Yadav <86115068+AnandY4d4v@users.noreply.github.com>
2022-06-05 21:25:20 +0530

    Time: 3 ms (61.21%), Space: 5.9 MB (33.10%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,22 +1,19 @@
-#include<limits.h>
 
+
+#include<bits/stdc++.h>
+#include<limits.h>
 class Solution {
 public:
     int reverse(int x) {
-        
-        int ans  = 0;
-        while( x != 0) {
-            
-            int digit = x % 10;
-            
-            if( (ans > INT_MAX/10) || (ans < INT_MIN/10)) {
-                return 0;
-            }
-            
-            ans = (ans * 10) + digit;
-            x = x / 10;
-            
-        }
+        int ans=0;
+        while(x !=0 ){
+        int ld = x % 10;
+             if ((ans > INT_MAX/10) || (ans < INT_MIN/10)){
+                 return 0;
+             }
+        ans = (ans * 10) + ld;
+        x = x / 10;     
+        }  
         return ans;
     }
 };
