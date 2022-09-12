9c85be602b64506cfaeafccb86b7e47c7a2d3e95
Photon <86115068+AnandY4d4v@users.noreply.github.com>
2022-09-12 16:55:01 +0530

    Time: 19 ms (12.07%), Space: 10.6 MB (27.39%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-         sort(tokens.begin(),tokens.end());
-        int i,j,n;
-        i=0;
-        j=n=tokens.size();
-        while(i < j){
-            if(power >= tokens[i])
-                power -= tokens[i++];
-            else if(i - (n-j) && j> i+1)
-                power += tokens[--j];
-            else
-                break;
+      sort(tokens.begin(),tokens.end());
+        if (tokens.size() == 0 || power < tokens[0]) return 0;
+        int left = 0, right = tokens.size() - 1, points = 0;
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                points++;
+                power -= tokens[left];
+                left++;
+            } else {
+                if (right - left > 1) {
+                    power += tokens[right];
+                    right--;
+                    points--;
+                } else break;
+            }
         }
-          return i-(n-j);
+        return points;
 
     }
 };
