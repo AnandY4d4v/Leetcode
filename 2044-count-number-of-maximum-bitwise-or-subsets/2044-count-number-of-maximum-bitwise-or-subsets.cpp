ea475e565eed57d51e8eae6e92e36594d0b47a14
Photon <86115068+AnandY4d4v@users.noreply.github.com>
2022-12-22 20:22:56 +0530

    Time: 104 ms (55.13%), Space: 307.6 MB (8.86%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     
-    int helper(int k,int n,vector<int> &nums,int i,int cur_or){
+    int helper(vector<vector<int>> &dp,vector<int> &nums,int i,int k,int cur_or,int n){
     if(i >= n) return cur_or == k;
-    return helper(k,n,nums,i + 1,cur_or) + helper(k,n,nums,i + 1,cur_or | nums[i]);
-    }
+    if(dp[i][cur_or] != -1) return dp[i][cur_or];
+    return dp[i][cur_or] = helper(dp,nums,i + 1,k,cur_or,n) + helper(dp,nums,i + 1,k,cur_or | nums[i],n);
+}
     
     int countMaxOrSubsets(vector<int>& nums) {
         int k = 0, n = nums.size();
         for(auto &i : nums){
             k |= i;
         }
-        return helper(k,n,nums,0,0);
+        vector<vector<int>> dp(n,vector<int>(k + 1,-1));
+         return helper(dp,nums,0,k,0,n);
     }
 };
