6b748f925b8c1bc1208a60d6855600825e2058f0
Photon <86115068+AnandY4d4v@users.noreply.github.com>
2023-01-11 23:30:44 +0530

    Added solution - LeetHub

---
+++ Left most and right most index - GFG/left-most-and-right-most-index.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> arr, long long x)
+    {
+        long long int n=arr.size();
+        long long int a= -1,b =-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x) a=i;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==x) b=i;
+        }
+       
+       pair<long , long> p = make_pair(b,a);
+       return p;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+
+// } Driver Code Ends
